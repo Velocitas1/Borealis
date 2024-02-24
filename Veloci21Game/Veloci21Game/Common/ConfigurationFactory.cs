f85faed6eb5035546dbf0523b35f733e183a5e93
Kelly Thompson <velocitas@gmail.com>
2024-02-24 08:53:10 -0600

    Framework.

---
+++ Veloci21Game/Veloci21Game/Common/ConfigurationFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Serilog;
+
+namespace Veloci21Game.Common;
+
+public static class ConfigurationFactory
+{
+    public static IConfigurationRoot Get()
+    {
+        var configurationRoot = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile($"{GetEliAssemblyShortName()}.json", true, true)
+            .AddJsonFile(Environment.GetEnvironmentVariable("SETTINGS_PATH") ?? "settings.json", true, true)
+            .Build();
+        return configurationRoot;
+    }
+
+    private static string GetEliAssemblyShortName()
+    {
+        var assemblyName = Assembly.GetEntryAssembly()?.GetName().Name?.Split('.').LastOrDefault();
+        return string.IsNullOrWhiteSpace(assemblyName) ? "appsettings" : assemblyName.ToLowerInvariant();
+    }
+
+    public static void LogDebugConfigurationDebugView(IConfigurationRoot configurationRoot)
+    {
+        foreach (var fileConfigurationProvider in configurationRoot.Providers.OfType<JsonConfigurationProvider>())
+            Log.Debug("Configuration Path: {Path}", fileConfigurationProvider.Source.Path);
+        Log.Debug("GetDebugView: {@GetDebugView}", configurationRoot.GetDebugView());
+    }
+}
