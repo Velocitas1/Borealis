9602244327e60822cb5f1e57c24c900f08f0486e
Kelly Thompson <velocitas@gmail.com>
2024-03-11 20:05:52 -0500

    Made a starter rock paper scisscors game.

---
+++ c#/RockPaperScissors/RockPaperScissors.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RockPaperScissors.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Player 1, Enter your name: ");
+            var player1 = new Person();
+            player1.Name = Console.ReadLine();
+            Console.WriteLine("Why hello there " + player1.Name);
+            Console.Write("Please enter Player's age: ");
+            while (!int.TryParse(Console.ReadLine(), out player1.Age))
+            {
+                Console.Write("You goof, that's not a number. Enter age: ");
+            }
+        }
+    }
+}
---
+++ c#/RockPaperScissors/RockPaperScissors/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace RockPaperScissors
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+    }
+}
